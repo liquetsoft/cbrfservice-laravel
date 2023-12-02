545756a50ea6b6f8d06f8449760f1a76056514a4
liquetsoft <liquetsoft@protonmail.com>
2023-12-03 00:34:08 +0100

    Add test console command for laravel insatllation

--- assets/TestCommand.php
+++ assets/TestCommand.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Liquetsoft\CbrfService\Laravel\CbrfDailyWrapper;
 
-class TestCommand extends Command
+final class TestCommand extends Command
 {
     /**
      * The name and signature of the console command.
@@ -28,7 +30,7 @@ class TestCommand extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(): void
     {
         dump($this->wrapper);
     }
